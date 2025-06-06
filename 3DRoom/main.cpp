d35c14281a734e9499ac797591900b01b78c5e2e
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-06-06 14:25:41 +0800

    Initial Commit

---
+++ 3DRoom/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3DRoom
+//
+//  Created by 劉丞恩 on 2025/6/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
